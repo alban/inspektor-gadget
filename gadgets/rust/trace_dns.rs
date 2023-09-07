57b72d6f5e3f3dbad41865ececb60c2245ba24d9
Alban Crequy <albancrequy@linux.microsoft.com>
2023-09-07 20:47:55 +0200

    run: add wasm from Rust
    Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

---
+++ gadgets/rust/trace_dns.rs
@@ -0,0 +1,34 @@
+use wapc_guest as wapc;
+
+#[no_mangle]
+pub fn wapc_init() {
+    wapc::register_function("column_name", column_name);
+}
+
+fn column_name(payload: &[u8]) -> wapc::CallResult {
+    let mut str = String::new();
+    let mut i = 0;
+    while i < payload.len() {
+        let length = payload[i] as usize;
+        if length == 0 {
+            break;
+        }
+        if i + 1 + length <= payload.len() {
+            str.push_str(std::str::from_utf8(&payload[i + 1..i + 1 + length]).unwrap());
+            str.push('.');
+        } else {
+            panic!("invalid payload");
+        }
+        i += 1 + length;
+    }
+
+    if str == "wikipedia.org." {
+        wapc::console_log(&format!(
+            "In WASM implemented in Rust: thank you for visiting Wikipedia: {}",
+            str
+        ));
+    }
+
+    Ok(str.into_bytes())
+}
+
