350429f3e761aaeaf5322847316ff80f94482b65
Alban Crequy <alban.crequy@gmail.com>
2025-07-15 19:03:58 +0200

    ttysnoop: implement real unit test
    Signed-off-by: Alban Crequy <alban.crequy@gmail.com>

--- gadgets/ttysnoop/test/unit/ttysnoop_test.go
+++ gadgets/ttysnoop/test/unit/ttysnoop_test.go
@@ -15,21 +15,106 @@
 package tests
 
 import (
+	"os"
+	"runtime"
 	"testing"
+	"time"
+
+	"github.com/creack/pty"
+	"github.com/stretchr/testify/require"
 
 	gadgettesting "github.com/inspektor-gadget/inspektor-gadget/gadgets/testing"
+	utilstest "github.com/inspektor-gadget/inspektor-gadget/internal/test"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-service/api"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/operators"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/testing/gadgetrunner"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/testing/utils"
 )
 
-func TestTraceTtysnoop(t *testing.T) {
-	// TODO: This is a dummy test to check that the gadget runs without errors.
-	// It should be extended to check that the gadget produces correct data.
-
-	// The current implementation relies on:
-	// - ITER_UBUF in enum iter_type introduced in Linux v6.0. See:
-	//   https://github.com/torvalds/linux/commit/fcb14cb1bdacec5b4374fe161e83fb8208164a85
-	// - ubuf_iovec in struct iov_iter introduced in Linux v6.4. See:
-	//   https://github.com/torvalds/linux/commit/747b1f65d39ae729b7914075899b0c82d7f667db
-	gadgettesting.MinimumKernelVersion(t, "6.4")
-
+func TestTtysnoopDummy(t *testing.T) {
+	// Keep this dummy test because it does not require a new version of vimto
+	// with "/dev/pts/ptmx". See:
+	// https://github.com/lmb/vimto/pull/26
+	gadgettesting.InitUnitTest(t)
+	gadgettesting.MinimumKernelVersion(t, "6.1")
 	gadgettesting.DummyGadgetTest(t, "ttysnoop")
+}
+
+type ExpectedTtysnoopEvent struct {
+	Proc utils.Process `json:"proc"`
+	Len  int           `json:"len"`
+	Buf  string        `json:"buf"`
+}
+
+type testDef struct {
+	runnerConfig *utilstest.RunnerConfig
+	text         string
+	validate     func(t *testing.T, info *utilstest.RunnerInfo, events []ExpectedTtysnoopEvent, text string)
+}
+
+func TestTtysnoopFull(t *testing.T) {
+	gadgettesting.InitUnitTest(t)
+
+	_, err := os.Stat("/dev/pts/ptmx")
+	if err != nil {
+		t.Skipf("Skipping test: /dev/pts/ptmx not found, this test requires a PTY device. Error: %v", err)
+	}
+
+	gadgettesting.MinimumKernelVersion(t, "6.1")
+
+	testCases := map[string]testDef{
+		"simple_write": {
+			runnerConfig: &utilstest.RunnerConfig{},
+			text:         "hello world",
+			validate: func(t *testing.T, info *utilstest.RunnerInfo, events []ExpectedTtysnoopEvent, text string) {
+				require.Len(t, events, 1, "Expected 1 event but got %d", len(events))
+				require.Equal(t, len(text), events[0].Len, "Expected Len %d, got %d", len(text), events[0].Len)
+				require.Equal(t, text, events[0].Buf, "Expected Args %q, got %q", text, events[0].Buf)
+			},
+		},
+	}
+
+	for name, testCase := range testCases {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			runner := utilstest.NewRunnerWithTest(t, testCase.runnerConfig)
+			onGadgetRun := func(gadgetCtx operators.GadgetContext) error {
+				utilstest.RunWithRunner(t, runner, func() error {
+					generateEventFromThread(t, testCase.text)
+					return nil
+				})
+				return nil
+			}
+			opts := gadgetrunner.GadgetRunnerOpts[ExpectedTtysnoopEvent]{
+				Image:          "ttysnoop",
+				Timeout:        5 * time.Second,
+				ParamValues:    api.ParamValues{},
+				MntnsFilterMap: utilstest.CreateMntNsFilterMap(t, runner.Info.MountNsID),
+				OnGadgetRun:    onGadgetRun,
+			}
+			gadgetRunner := gadgetrunner.NewGadgetRunner(t, opts)
+
+			gadgetRunner.RunGadget()
+			testCase.validate(t, runner.Info, gadgetRunner.CapturedEvents, testCase.text)
+		})
+	}
+}
+
+func generateEventFromThread(t *testing.T, text string) {
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	ptmx, tty, err := pty.Open()
+	if err != nil {
+		// Fails when /dev/pts is not mounted.
+		// See https://github.com/lmb/vimto/pull/26
+		t.Fatalf("Failed to open pty: %v", err)
+	}
+	defer ptmx.Close()
+	defer tty.Close()
+
+	_, err = ptmx.WriteString(text)
+	if err != nil {
+		t.Fatalf("Failed to write to pty: %v", err)
+	}
 }
