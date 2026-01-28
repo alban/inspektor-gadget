5848bad0dc580f4707cb35f8dba1c131c6ed6c0a
Alban Crequy <alban.crequy@gmail.com>
2026-01-28 16:39:30 +0100

    Reuse trace_malloc to demo uprobe on python with a clib
    sudo ig run ghcr.io/inspektor-gadget/gadget/trace_malloc:alban_ustack_otel3 --verify-image=false --collect-ustack --symbolizers otel-ebpf-profiler --collect-otel-stack=true --host --comm=pydemo.py -v
    
    pydemo.py            524968     524968 470507     0x561FCD1D04A0       1024                 allocate_memory
    
    Received OpenTelemetry trace (correlation ID 779760416722478, pid 524968, tid 524968):
      #0:  +0x7be6
      #1:  +0x44be
      #2:  +0x718d
      #3:  +0x9e59
      #4:  +0x9721
      #5:  +0x1b2792
      #6: level3 +0x0
        /home/alban/test/python-and-c-lib/./pydemo.py:21
      #7: level2 +0x0
        /home/alban/test/python-and-c-lib/./pydemo.py:26
      #8: level1 +0x0
        /home/alban/test/python-and-c-lib/./pydemo.py:30
      #9: main +0x0
        /home/alban/test/python-and-c-lib/./pydemo.py:35
      #10: <module> +0x0
        /home/alban/test/python-and-c-lib/./pydemo.py:39
      #11:  +0x1bae8c
      #12:  +0x1b7159
      #13:  +0x23d27b
      #14:  +0x25a722
      #15:  +0x256c89
      #16:  +0x26bfc1
      #17:  +0x26b7a7
      #18:  +0x26b467
      #19:  +0x265d77
      #20:  +0x22d43a
      #21:  +0x27b89
      #22:  +0x27c4a
      #23:  +0x1094

--- gadgets/trace_malloc/program.bpf.c
+++ gadgets/trace_malloc/program.bpf.c
@@ -15,7 +15,7 @@
 #define MAX_ENTRIES 10240
 
 enum memop {
-	malloc,
+	allocate_memory,
 	free,
 	calloc,
 	realloc,
@@ -94,6 +94,8 @@ static __always_inline int gen_alloc_exit(struct pt_regs *ctx,
 	u64 *size_ptr;
 	u64 size;
 
+	bpf_printk("gen_alloc_exit called with operation=%d, addr=0x%llx\n", operation, addr);
+
 	if (gadget_should_discard_data_current())
 		return 0;
 
@@ -122,159 +124,22 @@ static __always_inline int gen_alloc_exit(struct pt_regs *ctx,
 	return 0;
 }
 
-static __always_inline int gen_free_enter(struct pt_regs *ctx,
-					  enum memop operation, u64 addr)
-{
-	struct event *event;
-
-	if (gadget_should_discard_data_current())
-		return 0;
-
-	event = gadget_reserve_buf(&events, sizeof(*event));
-	if (!event)
-		return 0;
-
-	gadget_process_populate(&event->proc);
-	event->operation_raw = operation;
-	event->addr = addr;
-	event->size = 0;
-	event->timestamp_raw = bpf_ktime_get_ns();
-
-	gadget_get_user_stack(ctx, &event->ustack);
-
-	gadget_submit_buf(ctx, &events, event, sizeof(*event));
-
-	return 0;
-}
-
 /* common macros */
 #define PROBE_RET_VAL_FOR_ALLOC(func)                              \
-	SEC("uretprobe/libc:" #func)                               \
+	SEC("uretprobe//home/alban/test/python-and-c-lib/libmylib.so:" #func)                               \
 	int trace_uretprobe_##func(struct pt_regs *ctx)            \
 	{                                                          \
 		return gen_alloc_exit(ctx, func, PT_REGS_RC(ctx)); \
 	}
 
 /* malloc */
-SEC("uprobe/libc:malloc")
+SEC("uprobe//home/alban/test/python-and-c-lib/libmylib.so:allocate_memory")
 int BPF_UPROBE(trace_uprobe_malloc, size_t size)
 {
 	return gen_alloc_enter(size);
 }
 
-PROBE_RET_VAL_FOR_ALLOC(malloc)
+PROBE_RET_VAL_FOR_ALLOC(allocate_memory)
 
-/* free */
-SEC("uprobe/libc:free")
-int BPF_UPROBE(trace_uprobe_free, void *address)
-{
-	return gen_free_enter(ctx, free, (u64)address);
-}
-
-/* calloc */
-SEC("uprobe/libc:calloc")
-int BPF_UPROBE(trace_uprobe_calloc, size_t nmemb, size_t size)
-{
-	return gen_alloc_enter(nmemb * size);
-}
-
-PROBE_RET_VAL_FOR_ALLOC(calloc)
-
-/* realloc */
-SEC("uprobe/libc:realloc")
-int BPF_UPROBE(trace_uprobe_realloc, void *ptr, size_t size)
-{
-	gen_free_enter(ctx, realloc_free, (u64)ptr);
-	return gen_alloc_enter(size);
-}
-
-PROBE_RET_VAL_FOR_ALLOC(realloc)
-
-/* mmap */
-SEC("uprobe/libc:mmap")
-int BPF_UPROBE(trace_uprobe_mmap, void *address, size_t size)
-{
-	return gen_alloc_enter(size);
-}
-
-PROBE_RET_VAL_FOR_ALLOC(mmap)
-
-/* munmap */
-SEC("uprobe/libc:munmap")
-int BPF_UPROBE(trace_uprobe_munmap, void *address)
-{
-	return gen_free_enter(ctx, munmap, (u64)address);
-}
-
-/* posix_memalign */
-SEC("uprobe/libc:posix_memalign")
-int BPF_UPROBE(trace_uprobe_posix_memalign, void **memptr, size_t alignment,
-	       size_t size)
-{
-	u64 memptr64;
-	u32 tid;
-
-	tid = (u32)bpf_get_current_pid_tgid();
-	memptr64 = (u64)memptr;
-	bpf_map_update_elem(&memptrs, &tid, &memptr64, BPF_ANY);
-
-	return gen_alloc_enter(size);
-}
-
-SEC("uretprobe/libc:posix_memalign")
-int trace_uretprobe_posix_memalign(struct pt_regs *ctx)
-{
-	u64 *memptr64;
-	void *addr;
-	u32 tid;
-
-	tid = (u32)bpf_get_current_pid_tgid();
-
-	memptr64 = bpf_map_lookup_elem(&memptrs, &tid);
-	if (!memptr64)
-		return 0;
-	bpf_map_delete_elem(&memptrs, &tid);
-
-	if (bpf_probe_read_user(&addr, sizeof(void *), (void *)*memptr64))
-		return 0;
-
-	return gen_alloc_exit(ctx, posix_memalign, (u64)addr);
-}
-
-/* aligned_alloc */
-SEC("uprobe/libc:aligned_alloc")
-int BPF_UPROBE(trace_uprobe_aligned_alloc, size_t alignment, size_t size)
-{
-	return gen_alloc_enter(size);
-}
-
-PROBE_RET_VAL_FOR_ALLOC(aligned_alloc)
-
-/* valloc */
-SEC("uprobe/libc:valloc")
-int BPF_UPROBE(trace_uprobe_valloc, size_t size)
-{
-	return gen_alloc_enter(size);
-}
-
-PROBE_RET_VAL_FOR_ALLOC(valloc)
-
-/* memalign */
-SEC("uprobe/libc:memalign")
-int BPF_UPROBE(trace_uprobe_memalign, size_t alignment, size_t size)
-{
-	return gen_alloc_enter(size);
-}
-
-PROBE_RET_VAL_FOR_ALLOC(memalign)
-
-/* pvalloc */
-SEC("uprobe/libc:pvalloc")
-int BPF_UPROBE(trace_uprobe_pvalloc, size_t size)
-{
-	return gen_alloc_enter(size);
-}
-
-PROBE_RET_VAL_FOR_ALLOC(pvalloc)
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
